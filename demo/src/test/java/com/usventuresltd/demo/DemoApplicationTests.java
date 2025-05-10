5f42997f6b5cac1984e0309b5e0deba78c4554f5
Swamy Sivasubramaniyan <sswamyn@users.noreply.github.com>
2025-05-10 12:53:27 -0600

    adding the first Hello world example as a Test

--- demo/src/test/java/com/usventuresltd/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/usventuresltd/demo/DemoApplicationTests.java
@@ -1,13 +1,31 @@
 package com.usventuresltd.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+    @Autowired
+    private OpenAiChatModel openAiChatModel;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void simpleOpenAiQuery() {
+        // Create a chat client using the OpenAiChatModel.
+        // Using the ChatClient builder(model) to create a chat client.
+        ChatClient chatClient = ChatClient.builder(openAiChatModel).build();
+        String response = chatClient.prompt("Tell me mothers day joke")
+                .call()
+                .content();
+        System.out.println("Response: " + response);
+
     }
 
 }
